3963638c245784e7bd1e6e7ae9bfb27bc05f0757
tp <tomas.pinto94mil@gmail.com>
2023-11-10 20:06:43 -0300

    fixed error with checking request source

--- JS/middleware/jwtauth.js
+++ JS/middleware/jwtauth.js
@@ -2,21 +2,19 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 const JWTAuth = (req,res, next)=>{
-    source = req.useragent;
-    let token
-    if( req.isDesktop){
+    const source = req.useragent;
+    let token;
+    if( source.isDesktop){
         token = req.cookies.token;
     }else{
         token = req.query.token;
     }
     try {
-        console.log(token);
         const user = jwt.verify(token, process.env.MY_SECRET);
-        console.log(user);
         req.user = user;
         next();
     } catch (e) {
-        if(req.isDesktop){
+        if(source.isDesktop){
             res.clearCookie("token");
         }
         res.send("invalid token");
